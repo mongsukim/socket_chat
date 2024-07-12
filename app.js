5cf4969604fd5ac41d0f5a0921361bd37539b206
developermong2 <developermong2@gmail.com>
2024-07-12 12:31:08 +0900

    Fix : 오류 수정

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
 const app = express();
 app.use(cors());
 
-mongoose.connect(process.env.DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-    .then(() => console.log('connected to database'));
+mongoose.connect(process.env.DB, {}).then(() => console.log('connected to database'));
 
 module.exports = app;
--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import './App.css';
-import socket from './server';
-import { useEffect, useState } from 'react';
-import InputField from './components/InputField/InputField';
+import "./App.css";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    askUserName();
-  }, []);
-
-  const askUserName = () => {
-    const userName = prompt('당신의 이름을 입력하세요');
-    socket.emit('login', userName, (res) => {
-      if (res?.ok) {
-        setUser(res.data);
-      }
-    });
-  };
-
-  const sendMessage = () => {};
-
   return (
     <div>
-      <div className="App">
-        <InputField
-          message={message}
-          setMessage={setMessage()}
-          sendMessage={sendMessage}
-        />
-      </div>
+      <div className="App"></div>
     </div>
   );
 }
