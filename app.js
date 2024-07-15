ffcb5592cf175f2855cf24db712521a15274a14c
developermong2 <developermong2@gmail.com>
2024-07-15 11:26:08 +0900

    Feat : 기본 경로 라우트 핸들러 추가

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-mongoose.connect(process.env.DB, {}).then(() => console.log('connected to database'));
+// 데이터베이스 연결
+mongoose
+  .connect(process.env.DB, {})
+  .then(() => console.log('connected to database'))
+  .catch((err) => console.log('Error connecting to database:', err));
+
+// 기본 경로 라우트 핸들러 추가
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// 포트 설정 및 서버 시작
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
